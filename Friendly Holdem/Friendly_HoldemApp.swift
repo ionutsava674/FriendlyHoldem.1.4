92260b458412dcdd87fe71481187673ea0affa47
Ionut Sava <object.inspector@gmail.com>
2022-09-18 15:48:33 +0300

    Initial Commit

---
+++ Friendly Holdem/Friendly_HoldemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Friendly_HoldemApp.swift
+//  Friendly Holdem
+//
+//  Created by Ionut on 18.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Friendly_HoldemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
